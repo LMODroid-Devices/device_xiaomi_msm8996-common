be89a23dcaa20dd902a471a6830b583dfa4b2663
dianlujitao <dianlujitao@lineageos.org>
2020-11-24 15:14:20 +0800

    msm8996-common: Move configpanel and pocketmode to /product
    Change-Id: I80b9b0b9ada6be12b80938a75077c071927c6527
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -3,10 +3,11 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
-LOCAL_VENDOR_MODULE := true
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_REQUIRED_MODULES := XiaomiPocketMode
+LOCAL_PRODUCT_MODULE := true
 
 LOCAL_USE_AAPT2 := true
 
